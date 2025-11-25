71008ca7f5546fb130fcb1532cf809371f8943cd
a.lazarenko <an.afalin@yandex.ru>
2025-11-26 00:04:20 +0300

    [hw-4]: Описаны Replicaset и Service для Pod

--- src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
+++ src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
@@ -1,10 +1,17 @@
 package io.maksymdobrynin.snowflakegenerator
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/v1")
 class SnowflakeController(private val generator: Generator) {
+
+	private val log = LoggerFactory.getLogger(SnowflakeController::class.java)
+
 	@GetMapping("/next-id")
-	suspend fun generate(): Long = generator.nextId()
+	suspend fun generate(): Long {
+		log.info("Generated id...")
+		return generator.nextId()
+	}
 }
