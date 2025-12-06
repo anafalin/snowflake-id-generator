bd27f36c900d8ba6fcdbf2bafb8a6581cf73cd06
Anastasia Lazarenko <101412572+anafalin@users.noreply.github.com>
2025-12-06 15:15:24 +0300

    Описан replicaSet, service для балансировки, добавлено логирование пода
    Closes: hw-5

--- src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
+++ src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
@@ -1,17 +1,20 @@
 package io.maksymdobrynin.snowflakegenerator
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/v1")
-class SnowflakeController(private val generator: Generator) {
-
+class SnowflakeController(
+	@Value("\${kubernetes-settings.podName}") private var podName: String,
+	private val generator: Generator
+) {
 	private val log = LoggerFactory.getLogger(SnowflakeController::class.java)
 
 	@GetMapping("/next-id")
 	suspend fun generate(): Long {
-		log.info("Generated id...")
+		log.info("Generated id in pod=$podName")
 		return generator.nextId()
 	}
 }
